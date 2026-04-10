97d9f3fde4c36e12833d2c9b2d4ef108399e3d0a
Vineeth-2040 <vineethshankark1@gmail.com>
2026-04-10 23:05:18 +0530

    Tested closer to final version

--- AS_list.py
+++ AS_list.py
@@ -3,30 +3,28 @@ from datetime import datetime
 
 def get_directory_info(folder_path,max_depth=3,current_positon=0):
     result=[]
-    if os.path.exists(folder_path):
+    try :
         files=os.listdir(folder_path)
-        for file in files:
-            full_path=os.path.join(folder_path,file)
-            if os.path.isdir(full_path):
-                size=format_size(os.path.getsize(full_path))
-                created_time = datetime.fromtimestamp(os.path.getctime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
-                modified_time = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
-                result.append({"type":"dir","path":full_path+"/","name":file,"size":size,"created_time":created_time,"modified_time":modified_time})
-
-                if max_depth-1> current_positon :
-                    result.extend(get_directory_info(full_path,max_depth,current_positon+1))
-
-            else:
-                size=format_size(os.path.getsize(full_path))
-                created_time = datetime.fromtimestamp(os.path.getctime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
-                modified_time = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
-                result.append({"type":"file","path":full_path,"name":file,"size":size,"created_time":created_time,"modified_time":modified_time})
+    except PermissionError:
         return result
+            
+    for file in files:
+        full_path=os.path.join(folder_path,file)
+        if os.path.isdir(full_path):
+            size=format_size(os.path.getsize(full_path))
+            created_time = datetime.fromtimestamp(os.path.getctime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+            modified_time = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+            result.append({"type":"dir","path":full_path+"/","name":file,"size":size,"created_time":created_time,"modified_time":modified_time})
 
+            if current_positon < max_depth-1 :
+                result.extend(get_directory_info(full_path,max_depth,current_positon+1))
 
-    else : 
-        return result
-
+        else:
+            size=format_size(os.path.getsize(full_path))
+            created_time = datetime.fromtimestamp(os.path.getctime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+            modified_time = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+            result.append({"type":"file","path":full_path,"name":file,"size":size,"created_time":created_time,"modified_time":modified_time})
+    return result
 
 
 def format_size(bytes):
@@ -41,7 +39,7 @@ def format_size(bytes):
     
 
 
-path= r"E:\HRTF"
+path= r"E:"
 result=get_directory_info(path,1)
 for item in result:
     print(f"{item['type'].upper()}: {item['name']} - Size: {item['size']} - Created: {item['created_time']} - Modified: {item['modified_time']}")
