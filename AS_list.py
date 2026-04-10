a52506012dbd79724078b4292d0825a0c6c494d1
Vineeth-2040 <vineethshankark1@gmail.com>
2026-04-10 22:57:46 +0530

    Tested issue with recurvise call - it worked -issue was i throwed the result off so

--- AS_list.py
+++ AS_list.py
@@ -1,8 +1,7 @@
 import os
-import shutil
+from datetime import datetime
 
-def get_directory_info(folder_path,max_depth=3):
-    current_positon=0
+def get_directory_info(folder_path,max_depth=3,current_positon=0):
     result=[]
     if os.path.exists(folder_path):
         files=os.listdir(folder_path)
@@ -10,18 +9,17 @@ def get_directory_info(folder_path,max_depth=3):
             full_path=os.path.join(folder_path,file)
             if os.path.isdir(full_path):
                 size=format_size(os.path.getsize(full_path))
-                created_time=os.path.getctime(full_path)
-                modified_time=os.path.getmtime(full_path)
+                created_time = datetime.fromtimestamp(os.path.getctime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+                modified_time = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
                 result.append({"type":"dir","path":full_path+"/","name":file,"size":size,"created_time":created_time,"modified_time":modified_time})
 
                 if max_depth-1> current_positon :
-                    current_positon+=1
-                    get_directory_info(full_path,max_depth)
+                    result.extend(get_directory_info(full_path,max_depth,current_positon+1))
 
             else:
                 size=format_size(os.path.getsize(full_path))
-                created_time=os.path.getctime(full_path)
-                modified_time=os.path.getmtime(full_path)
+                created_time = datetime.fromtimestamp(os.path.getctime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+                modified_time = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
                 result.append({"type":"file","path":full_path,"name":file,"size":size,"created_time":created_time,"modified_time":modified_time})
         return result
 
@@ -43,6 +41,7 @@ def format_size(bytes):
     
 
 
-path= "E:\HRTF"
-result=get_directory_info(path)
-print(result)
+path= r"E:\HRTF"
+result=get_directory_info(path,1)
+for item in result:
+    print(f"{item['type'].upper()}: {item['name']} - Size: {item['size']} - Created: {item['created_time']} - Modified: {item['modified_time']}")
