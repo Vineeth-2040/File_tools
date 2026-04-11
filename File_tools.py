5323d42d7c1cd4434526a0c7864440aff40b7f96
Vineeth-2040 <vineethshankark1@gmail.com>
2026-04-11 21:32:22 +0530

    before testing File_tools.py added differnt file funtion in to one

--- File_tools.py
+++ File_tools.py
@@ -1,5 +1,42 @@
 import os 
 from AS_list import get_directory_info
+from datetime import datetime
+
+def get_directory_info(folder_path,max_depth=1,current_positon=0):
+    result=[]
+    try :
+        files=os.listdir(folder_path)
+    except PermissionError:
+        return result
+    except FileNotFoundError:
+        return result
+            
+    for file in files:
+        full_path=os.path.join(folder_path,file)
+        created_time = datetime.fromtimestamp(os.path.getctime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+        modified_time = datetime.fromtimestamp(os.path.getmtime(full_path)).strftime("%Y-%m-%d %H:%M:%S")
+
+        if os.path.isdir(full_path):
+            result.append({"type":"dir","path":full_path+"/","name":file,"ext":None,"size":None,"created_time":created_time,"modified_time":modified_time})
+
+            if current_positon < max_depth-1 :
+                result.extend(get_directory_info(full_path,max_depth,current_positon+1))
+
+        else:
+            size=format_size(os.path.getsize(full_path))
+            ext=os.path.splitext(file)[1].lower()
+            result.append({"type":"file","path":full_path,"name":file,"ext":ext,"size":size,"created_time":created_time,"modified_time":modified_time,})
+    return result
+
+def format_size(bytes):
+    if bytes < 1024:
+        return f"{bytes} B"
+    elif bytes < 1024 ** 2:
+        return f"{bytes / 1024:.1f} KB"
+    elif bytes < 1024 ** 3:
+        return f"{bytes / 1024**2:.1f} MB"
+    else:
+        return f"{bytes / 1024**3:.1f} GB"
 
 
 def search_filesystem(root, query=None, ext=None, max_depth=3, file_type=None):
